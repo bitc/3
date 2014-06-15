941c9c8618aec1fb034a1d4de4b70bc7f43374e0
kramekb <>
2014-06-15 15:40:55 +0300

    Modify `ln' program to add `-s' command line option

--- ln.c
+++ ln.c
@@ -5,11 +5,20 @@
 int
 main(int argc, char *argv[])
 {
-  if(argc != 3){
-    printf(2, "Usage: ln old new\n");
+  if(!(argc == 3 || (argc == 4 && strcmp(argv[1], "-s") == 0))){
+    printf(2, "Usage: ln [-s] old new\n");
     exit();
   }
-  if(link(argv[1], argv[2]) < 0)
-    printf(2, "link %s %s: failed\n", argv[1], argv[2]);
+
+  if(argc == 3) {
+    if(link(argv[1], argv[2]) < 0)
+      printf(2, "link %s %s: failed\n", argv[1], argv[2]);
+  }
+  else{
+    if(symlink(argv[2], argv[3]) < 0) {
+      printf(2, "link -s %s %s: failed\n", argv[1], argv[2]);
+    }
+  }
+
   exit();
 }
